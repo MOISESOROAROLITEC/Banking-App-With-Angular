aaff156d6bfc539ebca517391ce05c5370d51e4a
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-10 13:58:22 +0000

    change app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'banckingWithAngular'`, () => {
+  it(`should have as title 'bankingWithAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('banckingWithAngular');
+    expect(app.title).toEqual('bankingWithAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('banckingWithAngular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bankingWithAngular app is running!');
   });
 });
