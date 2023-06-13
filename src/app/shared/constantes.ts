d2eb9ef042244b63109b7a7acc9bf51a8eee264e
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-13 16:56:20 +0000

    Added a login and sign-up interface

--- src/app/shared/constantes.ts
+++ src/app/shared/constantes.ts
@@ -1,7 +1,19 @@
 export const baseUrl = "http://localhost:5432"
 
-export class UserDatas {
-  name: string = "";
-  password: string = ""
-  email: string = ""
+export interface UserDatasSignUp {
+  name: string,
+  password: string,
+  email: string,
+}
+
+export interface UserDatasLogin {
+  email: string,
+  password: string
+}
+
+export interface UserDatas {
+  name: string
+  password?: string
+  email: string
+  token?: string
 }
