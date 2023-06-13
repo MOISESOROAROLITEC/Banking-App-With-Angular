da495330abaca053fec0f0b4876b507677dc4f05
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-13 16:35:45 +0000

    creating login page

---
+++ src/app/features/auth/components/login/login.component.ts
@@ -0,0 +1,62 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { UserDatas } from 'src/app/shared/constantes';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  signUpForm: FormGroup;
+  requestErroMessage = ""
+  emailAlreadyExistError = ""
+  hide = true
+  check = false
+  userData = new UserDatas()
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.signUpForm = this.formBuilder.group({
+      username: ['', [Validators.maxLength(50), Validators.minLength(4)]],
+      email: ['', [Validators.email]],
+      password: ['', [Validators.pattern("^.{8,50}$")]],
+    });
+  }
+
+  changeCheckedState() {
+    this.check = !this.check
+  }
+
+  onInput() {
+    this.requestErroMessage = ""
+    this.emailAlreadyExistError = ""
+  }
+
+  onSubmit() {
+    this.userData.name = this.signUpForm.get('username')?.value
+    this.userData.email = this.signUpForm.get('email')?.value
+    this.userData.password = this.signUpForm.get('password')?.value
+    this.authService.createUser(this.userData)?.subscribe(
+      (response) => {
+        if (response instanceof HttpErrorResponse) {
+          if (response.status == 401) {
+            this.emailAlreadyExistError = "Cette adress email est déjàs utilisé"
+          } else if (response.status == 400) {
+            this.emailAlreadyExistError = "Le format de l'email est incorrect"
+          }
+        } else {
+          this.router.navigate(["/login"])
+        }
+      }
+    )
+  }
+
+}
