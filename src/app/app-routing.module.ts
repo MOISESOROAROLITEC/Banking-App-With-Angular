16a14c85e64b6b1b5086bde24bc3c10213e62075
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-14 16:40:37 +0000

    add auth module and dashboard module as lazy load modue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", redirectTo: "sign-up", pathMatch: "full" },
-  { path: "", loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule), pathMatch: "full" },
-  { path: "", loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) }
+  { path: "", redirectTo: "auth/sign-up", pathMatch: "full" },
+  { path: "auth", loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
+  { path: "dashboard", loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) }
 ];
 
 @NgModule({
