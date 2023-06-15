545f5886224d9f0a727691de64adfd4ab44db09b
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-15 10:10:48 +0000

    remove catchError

--- src/app/features/auth/service/auth.service.ts
+++ src/app/features/auth/service/auth.service.ts
@@ -12,29 +12,12 @@ export class AuthService {
 
   createUser(userDatas: UserDatasSignUp) {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
-
     return this.http.post<UserDatas>(`${baseUrl}/user/create`, userDatas, httpOptions)
-      .pipe(
-        catchError((err: Error) => {
-          return of(err)
-        })
-      )
-
-
   }
 
   login(userDatas: UserDatasLogin) {
     const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
-    try {
-      return this.http.post(`${baseUrl}/auth/login`, userDatas, httpOptions)
-        .pipe(
-          catchError((err: Error) => {
-            return of(err)
-          })
-        )
-    } catch (error) {
-      return of(error)
-    }
+    return this.http.post(`${baseUrl}/auth/login`, userDatas, httpOptions)
   }
 
 
