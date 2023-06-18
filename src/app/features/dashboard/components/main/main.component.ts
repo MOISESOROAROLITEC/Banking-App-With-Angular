f38dbd4c7b52e366d39e0a638cb21a9fc7fcf8af
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-18 00:49:37 +0000

    account is recived on dashboard

--- src/app/features/dashboard/components/main/main.component.ts
+++ src/app/features/dashboard/components/main/main.component.ts
@@ -1,40 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { changeUserName } from 'src/app/features/auth/store/user.actions';
 import { getUserDatas, getUserFirstName, getUserInitial, getUserLastNames } from 'src/app/features/auth/store/user.selector';
-import { UserDatas } from 'src/app/shared/constantes/constantes';
+import { Account, UserAccounts, UserDatas } from 'src/app/shared/constantes/constantes';
+import { getUserAccount, getUserAccounts, getUserSubAccount } from '../../store/dashboard.selector';
+import { getUserAccountsAction } from '../../store/dashboard.actions';
+import { DashboardService } from '../../services/dashboard.service';
 
 
 
 @Component({
-	selector: 'app-main',
-	templateUrl: './main.component.html',
-	styleUrls: ['./main.component.scss']
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-	userDatas$: Observable<UserDatas>
-	userInitial$: Observable<string>
-	firstName$: Observable<string>
-	lastNames$: Observable<string>
+  userDatas$: Observable<UserDatas>
+  userInitial$: Observable<string>
+  firstName$: Observable<string>
+  lastNames$: Observable<string>
+  userAccounts$: Observable<UserAccounts>
+  userAccount$: Observable<Account>
+  userSubAccount$: Observable<Array<Account>>
 
-	constructor(
-		private store: Store<UserDatas>,
-	) {
-		this.userDatas$ = this.store.select(getUserDatas)
-		this.userInitial$ = this.store.select(getUserInitial)
-		this.firstName$ = this.store.select(getUserFirstName)
-		this.lastNames$ = this.store.select(getUserLastNames)
-	}
+  constructor(
+    private store: Store<UserDatas>,
+    private dashboardService: DashboardService
+  ) {
+    this.userDatas$ = this.store.select(getUserDatas)
+    this.userInitial$ = this.store.select(getUserInitial)
+    this.firstName$ = this.store.select(getUserFirstName)
+    this.lastNames$ = this.store.select(getUserLastNames)
+    this.userAccounts$ = this.store.select(getUserAccounts)
+    this.userAccount$ = this.store.select(getUserAccount)
+    this.userSubAccount$ = this.store.select(getUserSubAccount)
+  }
 
-	ngOnInit(): void {
-		this.userDatas$.subscribe({
-			next: (value) => { }
-		})
-	}
+  ngOnInit() {
+    this.store.dispatch(getUserAccountsAction());
+  }
 
-	click() {
-		this.store.dispatch(changeUserName({ newName: "new name" }))
-	}
+  onDisconnect() {
+    this.dashboardService.disconnectUser()
+  }
 
 }
