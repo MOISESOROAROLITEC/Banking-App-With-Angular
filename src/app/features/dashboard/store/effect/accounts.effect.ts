b54b407383277dca0af9e3615bbf07654d01dad3
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-21 06:19:16 +0000

    move dashboard service

--- src/app/features/dashboard/store/effect/accounts.effect.ts
+++ src/app/features/dashboard/store/effect/accounts.effect.ts
@@ -2,81 +2,81 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, exhaustMap, map } from 'rxjs/operators';
 import { of } from 'rxjs';
-import { DashboardService } from '../services/dashboard.service';
+import { DashboardService } from '../../services/dashboard.service';
 import { ToastService } from 'src/app/shared/services/toast/toast.service';
 import {
-  getUserAccountsAction,
-  getUserAccountsSucceed,
-  getUserAccountsFailed,
-  createSaveAccountAction,
-  createSaveAccountSucceed,
-  createSaveAccountFailed,
-  createBlockedAccountAction,
-  createBlockedAccountSucceed,
-  createBlockedAccountFailed,
+	getUserAccountsAction,
+	getUserAccountsSucceed,
+	getUserAccountsFailed,
+	createSaveAccountAction,
+	createSaveAccountSucceed,
+	createSaveAccountFailed,
+	createBlockedAccountAction,
+	createBlockedAccountSucceed,
+	createBlockedAccountFailed,
 } from '../actions/accounts.actions';
 import { Store } from '@ngrx/store';
 
 @Injectable()
 export class DashboardEffects {
-  getUserAccountsEffect$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(getUserAccountsAction.type),
-      exhaustMap(() => this.dashboardService.getUserAccounts()
-        .pipe(
-          map((response) => {
-            return ({ type: getUserAccountsSucceed.type, userAccounts: response })
-          }),
-          catchError((error) => {
-            this.toast.error(error.error.message)
-            return of({ type: getUserAccountsFailed.type, message: error.error.message })
-          })
-        )
-      )
-    )
-  )
+	getUserAccountsEffect$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(getUserAccountsAction.type),
+			exhaustMap(() => this.dashboardService.getUserAccounts()
+				.pipe(
+					map((response) => {
+						return ({ type: getUserAccountsSucceed.type, userAccounts: response })
+					}),
+					catchError((error) => {
+						this.toast.error(error.error.message)
+						return of({ type: getUserAccountsFailed.type, message: error.error.message })
+					})
+				)
+			)
+		)
+	)
 
-  createSaveAccountEffect$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(createSaveAccountAction.type),
-      exhaustMap(({ parentIban }) => this.dashboardService.createSaveAccounts(parentIban)
-        .pipe(
-          map((response) => {
-            this.store.dispatch(getUserAccountsAction());
-            return ({ type: createSaveAccountSucceed.type, subAccount: response })
-          }),
-          catchError((error) => {
-            this.toast.error(error.error.message)
-            return of({ type: createSaveAccountFailed.type, message: error.error.message })
-          })
-        )
-      )
-    )
-  )
+	createSaveAccountEffect$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(createSaveAccountAction.type),
+			exhaustMap(({ parentIban }) => this.dashboardService.createSaveAccounts(parentIban)
+				.pipe(
+					map((response) => {
+						this.store.dispatch(getUserAccountsAction());
+						return ({ type: createSaveAccountSucceed.type, subAccount: response })
+					}),
+					catchError((error) => {
+						this.toast.error(error.error.message)
+						return of({ type: createSaveAccountFailed.type, message: error.error.message })
+					})
+				)
+			)
+		)
+	)
 
-  createBlockedAccountEffect$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(createBlockedAccountAction.type),
-      exhaustMap(({ parentIban }) => this.dashboardService.createBlockedAccounts(parentIban)
-        .pipe(
-          map((response) => {
-            this.store.dispatch(getUserAccountsAction());
-            return ({ type: createBlockedAccountSucceed.type, subAccount: response })
-          }),
-          catchError((error) => {
-            this.toast.error(error.error.message)
-            return of({ type: createBlockedAccountFailed.type, message: error.error.message })
-          })
-        )
-      )
-    )
-  )
+	createBlockedAccountEffect$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(createBlockedAccountAction.type),
+			exhaustMap(({ parentIban }) => this.dashboardService.createBlockedAccounts(parentIban)
+				.pipe(
+					map((response) => {
+						this.store.dispatch(getUserAccountsAction());
+						return ({ type: createBlockedAccountSucceed.type, subAccount: response })
+					}),
+					catchError((error) => {
+						this.toast.error(error.error.message)
+						return of({ type: createBlockedAccountFailed.type, message: error.error.message })
+					})
+				)
+			)
+		)
+	)
 
-  constructor(
-    private actions$: Actions,
-    private dashboardService: DashboardService,
-    private toast: ToastService,
-    private store: Store
-  ) { }
+	constructor(
+		private actions$: Actions,
+		private dashboardService: DashboardService,
+		private toast: ToastService,
+		private store: Store
+	) { }
 
 }
