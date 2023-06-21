6817595bfa372c355b1dde88cbd352c45168889f
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-21 06:10:03 +0000

    remove user info folder

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserInfoComponent } from './components/user-info/user-info.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
 import { MainComponent } from './components/main/main.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -36,9 +35,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     MainComponent,
-    UserInfoComponent,
-    UserInfoComponent,
-    TransactionsComponent
+    TransactionsComponent,
   ],
   imports: [
     CommonModule,
